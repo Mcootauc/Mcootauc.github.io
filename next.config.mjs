a8c272ffdd4b390b709cb2fc249d5e1b37871e8b
Mitchell Cootauco <Mcootauc@lion.lmu.edu>
2025-06-10 14:16:56 -0700

    update image src path

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/my-app',
     output: 'export', // tells Next.js to write static files to /out
     // trailingSlash: true // optional but avoids extra Nginx‑style rewrites
 };
