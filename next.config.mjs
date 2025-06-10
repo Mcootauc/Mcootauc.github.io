872d35f137474be84b90afbe894cba6e0749e000
Mitchell Cootauco <Mcootauc@lion.lmu.edu>
2025-06-10 14:25:17 -0700

    revert changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-    basePath: isProd ? '/personal-portfolio' : '',
     output: 'export', // tells Next.js to write static files to /out
     // trailingSlash: true // optional but avoids extra Nginx‑style rewrites
 };
